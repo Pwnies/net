43144c53a4b2fc1f6a76df66879d5afaf331747b
Tethys Svensson <freaken@freaken.dk>
2020-08-26 11:10:45 +0200

    Add bash completion to nix

--- derivation.nix
+++ derivation.nix
@@ -8,11 +8,12 @@ in runCommand "net" {
     homepage = "https://github.com/Pwnies/net/";
     description = "Super lightweight network manager";
     license = licenses.unlicense;
-    maintainers = with maintainers; [ kristoff3r ];
+    maintainers = with maintainers; [ kristoff3r TethysSvensson ];
   };
 } ''
-  mkdir -p $out/bin
+  mkdir -p $out/bin $out/share/bash-completion/completions
   cp ${./net} $out/bin/net
+  cp ${./_net_bash_completion} $out/share/bash-completion/completions/net
   # Fix the path now to make patchShebangs do the right thing
   PATH=${path} patchShebangs $out/bin/net
   sed -i "1 a import os; os.environ['PATH'] = '${path}:' + os.environ['PATH']" $out/bin/net
